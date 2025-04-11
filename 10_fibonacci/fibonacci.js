4958b5e7829ca87aa0b2b059c8329b2b0edf35f1
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-04-11 10:21:43 -0400

    Completed exercises 8-12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(seqNumber) {
+  seqNumber = Number(seqNumber)
 
+  if (seqNumber === 0){
+    return 0;
+  } else if (seqNumber < 0){
+    return "OOPS";
+  } else if (seqNumber === 1) {
+    return 1;
+  } else {
+    let firstNumber = 0;
+    let secondNumber = 1;
+    let sum;
+
+    for (let i = 2; i <= seqNumber; i++) {
+      sum = firstNumber + secondNumber;
+      firstNumber = secondNumber;
+      secondNumber = sum;
+    }
+      return sum;
+  }
 };
 
 // Do not edit below this line
